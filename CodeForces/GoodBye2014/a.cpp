dd830d7ff20fde2015902b63d3fbe7f211b6f073
Dalu feng <fengdalu@gmail.com>
2014-12-31 01:57:47 +0800

    final code

--- CodeForces/GoodBye2014/a.cpp
+++ CodeForces/GoodBye2014/a.cpp
@@ -3,9 +3,28 @@
 #include <cstring>
 #include <algorithm>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
+int n;
+int f[44444];
+
 int main()
-{
+{   
+    int t;
+    scanf("%d%d", &n, &t);
+    for(int i = 0; i < n - 1; i++)
+    {
+        scanf("%d", &f[i]);
+    }  
+   int p = 0;
+   t--;
+    while(p < t) 
+    {
+        p += f[p];
+    }
+    if(p == t) puts("YES");
+    else puts("NO");
+    return 0;
 }
