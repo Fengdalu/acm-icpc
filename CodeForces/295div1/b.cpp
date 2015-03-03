a60d7d48f85653f2bd8e2f3638ce12ddc5c7aa43
Dalu feng <fengdalu@gmail.com>
2015-03-03 12:01:37 +0800

    2015年3月3日12:01:38 CF295div1，B，WA

--- CodeForces/295div1/b.cpp
+++ CodeForces/295div1/b.cpp
@@ -4,42 +4,51 @@
 #include <iostream>
 #include <algorithm>
 #define MAXM 600000
-#define MOD 1000000009
 #define MAXN 200000
 
 using namespace std;
 
 struct P
 {
-	int x, y, n;
-
+	int x, y, id;
+	int down[3], up[3];
 }pt[MAXN];
 
-bool operator < (const P &a, const P &b)
+struct cmp
 {
-	return a.n < b.n;
-}
+	bool operator() (const int &a, const int &b)
+	{
+		return pt[a].id - pt[b].id;
+	}
+};
 
-int ind[MAXN], t[MAXM], nt[MAXM];
-int c[MAXN];
 int BG[MAXN];
 int cnt = 0, n;
 int cc = 0;
-set<P>s;
+set<int, cmp>s;
+
+bool only(int x)
+{
+	P &p = pt[x];
+	int num = 0;
+	for(int i = 0; i < 3; i++) if(p.down[i] != -1) num++;
+	if(num == 1) return true;
+	return false;
+}
+
+bool can(int x)
+{
+	P &p = pt[x];
+	for(int i = 0; i < 3; i++)
+		if(only(p.up[i])) return false;
+	return true;
+}
 
 
 int cmp(P a, P b)
 {
 	if(a.y == b.y) return a.x < b.x;
-	return a.y < b.y;
-}
-
-void add(int a, int b)
-{
-	cnt++;
-	t[cnt] = b;
-	nt[cnt] = ind[a];
-	ind[a] = cnt;
+	return a.y > b.y;
 }
 
 int find(int l, int r, int c)
@@ -55,13 +64,74 @@ int find(int l, int r, int c)
     else return -1;
 }
 
+void link(int up, int down)
+{
+	P &u = pt[up], &d = pt[down];
+	for(int i = 0; i < 3; i++) if(u.down[i] == -1) { u.down[i] = down; break; }
+	for(int i = 0; i < 3; i++) if(d.up[i] == -1) { d.up[i] = up; break; }
+}
+
+void unlink(int up, int down)
+{
+	P &d = pt[down], &u = pt[up];
+	for(int i = 0; i < 3; i++) if(d.up[i] == up) { d.up[i] = -1; break; }
+	for(int i = 0; i < 3; i++) if(d.down[i] == down) { d.down[i] = -1; break; }
+}
+
+void del(int x)
+{
+	P &p = pt[x];
+	for(int i = 0; i < 3; i++)
+	if(p.down[i] != -1)
+	{
+		int d = p.down[i];
+		unlink(x, d);
+		if(can(d)) { s.insert(d); }
+	}
+}
+
+void gen()
+{
+	for(int ii = 0; ii < cc; ii++)
+	if(pt[BG[ii]].y == pt[BG[ii + 1]].y + 1)		
+	{
+		int l = BG[ii + 1], r = BG[ii + 2] - 1;
+		for(int i = BG[ii]; i < BG[ii + 1]; i++)
+		{
+			int x, t;
+			x = pt[i].x;
+			t = find(l, r, x);
+			if(t != -1) link(i, t);
+			
+			x = pt[i].x;
+			t = find(l, r, x + 1);
+			if(t != -1) link(i, t);
+			
+			x = pt[i].x;
+			t = find(l, r, x - 1);
+			if(t != -1) link(i, t);
+		}
+	}
+}
+
+void operator << (ostream& out, const P& p)
+{
+	out << "id: " << p.id << endl;
+	out << "x: " << p.x << ", y: " << p.y << endl;
+	out << "To: " << endl;
+	for(int i = 0; i < 3; i++) 
+	if(p.down[i] != -1) out << p.down[i] << endl;
+	out << endl;
+}
+
 int main()
 {
 	scanf("%d", &n);
 	for(int i = 0; i < n; i++)
 	{
 		scanf("%d%d", &pt[i].x, &pt[i].y);
-		pt[i].n = i;
+		for(int j = 0; j < 3; j++) pt[i].up[j] = pt[i].down[j] = -1;
+		pt[i].id = i;
 	}
 	sort(pt, pt + n, cmp);
 	cc = 0;
@@ -75,58 +145,35 @@ int main()
 		
 	cc++;
 	BG[cc] = n;		
-	cnt = 0;
-	memset(c, 0, sizeof c);
-	for(int i = 0; i < n; i++) ind[i] = -1;	
-	
-	for(int ii = 0; ii < cc - 1; ii++)
-		if(pt[BG[ii]].y == pt[BG[ii + 1]].y - 1)
-		for(int i = BG[ii]; i < BG[ii + 1]; i++)
-		{
-			int t;
-			t = find(BG[ii + 1], BG[ii + 2] - 1, pt[i].x);
-			if(t != -1) { add(pt[t].n, pt[i].n); c[pt[i].n]++; }
-			
-			t = find(BG[ii + 1], BG[ii + 2] - 1, pt[i].x + 1);
-			if(t != -1) { add(pt[t].n, pt[i].n); c[pt[i].n]++; }
-			
-			t = find(BG[ii + 1], BG[ii + 2] - 1, pt[i].x - 1);
-			if(t != -1) { add(pt[t].n, pt[i].n); c[pt[i].n]++; }
-		}
-		
+	gen();
 	s.clear();
+	//for(int i = 0; i < n; i++) cout << pt[i];
 	for(int i = 0; i < n; i++)
-	if(c[pt[i].n] != 1)
-	{
-		cout << pt[i].n << endl;
-		s.insert(pt[i]);
-	}
-	cout << "Hehe" << endl;
+		if(can(i)) 
+		{
+			s.insert(i);
+		}
+	
 	long long ans = 0;
-	return 0;
+	long long MOD = 1000000009;
+	
+	
 	while(!s.empty())
 	{
-		cout << "Bg" << endl;
-		set<P>::iterator p = s.begin();
-		//ans = (ans * n % MOD + p->n);
-		for(int k = ind[p->n]; k != -1; k = nt[k])
-		{
-			c[pt[t[k]].n]--;
-			if(c[pt[t[k]].n] != 1 && s.find(pt[t[k]]) != s.end()) s.insert(pt[t[k]]);
-		}
-		s.erase(p);
+		int t;
 		
-		/*
-		p = s.end() - 1;
-		//ans = (ans * n % MOD + p->n);
-		for(int k = ind[p->n]; k != -1; k = nt[k])
-		{
-			c[pt[t[k]].n]--;
-			if(c[pt[t[k]].n] != 1 && s.find(pt[t[k]]) != s.end()) s.insert(pt[t[k]]);
-		}
-		s.erase(p);		
-		*/
+		t = *s.begin();
+		s.erase(s.begin()); 
+		del(t);
+		ans = (ans * n + pt[t].id) % MOD;		
+	
+	
+		t = *(--s.end());
+		s.erase(--s.end()); 
+		del(t);
+		ans = (ans * n + pt[t].id) % MOD;				
 	}
-	cout << ans << endl;
+	printf("%I64d\n", ans);
+	
 	return 0;
 } 
