5fbcbc498f53d6d7a1e8605919e05e81c57eb1d9
Dalu feng <fengdalu@gmail.com>
2015-03-05 13:57:37 +0800

    2015年3月5日13:57:39 CF157 E Coding

---
+++ CodeForces/157/258E.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+using namespace std;
+#define LL long long
+#define N 100000
+#define M 200000
+#define PB(x) push_back(x)
+#define SZ size()
+
+
+struct obj
+{
+	int a, b;
+	int l, r;
+	int s, add;
+}T[N];
+
+vector<int>IND[N];
+int n, m;
+int ind[N], t[M], nt[M];
+int st[N], ed[N];
+int cnt, cnt1;
+int time;
+
+void add(int a, int b)
+{
+	cnt++;
+	t[cnt] = b;
+	nt[cnt] = ind[a];
+	ind[a] = cnt;
+}
+
+void BD(int x)
+{		
+	time++;
+	st[x] = time;
+	for(int i = 0; i < IND[x].SZ; i++)
+	{
+		add(i, IND[x][i]);
+		BD(IND[x][i]);
+	}
+	ed[x] = time;
+}
+
+int MT(int a, int b)
+{
+	int rt = ++cnt1;
+	T[rt].a = a; T[rt].b = b;
+	T[rt].s = T[rt].add = 0;
+	T[rt].l = T[rt].r = 0;
+	if(a < b)
+	{
+		int m = (a + b) >> 1;
+		T[rt].l = MT(a, m);
+		T[rt].r = MT(m + 1, b);
+	}
+	return rt;
+} 
+
+void update(int rt)
+{
+	if(T[rt].add) T[rt].s = (T[rt].b - T[rt].a + 1);
+	int l = T[rt].l, r = T[rt].r;
+	if(l && r) T[rt].s = T[l].s + T[r].s;
+}
+
+void draw(int rt, int c)
+{	
+	T[rt].add += c;
+	update(rt);
+}
+
+
+void inc(int rt, int a, int b, int c)
+{	
+	if(a <= T[rt].a && T[rt].b <= b) draw(rt, c);
+	else 
+	{
+		int m = (a + b) >> 1;
+		if(a <= m) inc(T[rt].l, a, b, c);
+		if(b > m) inc(T[rt].r, a, b, c);
+		update(rt);
+	}
+}
+
+
+int main()
+{
+	scanf("%d%d", &n, &m);
+	for(int i = 1; i <= n; i++) IND[i].clear();
+	for(int i = 1; i < n; i++) 
+	{
+		int x, y;
+		scanf("%d%d", &x, &y);
+		IND[x].PB(x); IND[x].PB(y);
+	}
+	for(int i = 1; i <= n; i++) ind[i] = -1;
+	time = 0; cnt = 0;
+	BD(1);
+	cnt1 = 0;
+	int rt = MT(1, n);
+}
