5f35a2e4f5da5a90917c74ce2ffca592817030e0
Dalu Feng <fengdalu@gmail.com>
2015-03-11 20:40:15 +0800

    Hdu2971 WA

--- Hdu/2971.cpp
+++ Hdu/2971.cpp
@@ -18,29 +18,30 @@ struct Matrix
 	}	
 	Matrix& set(int i, int j, long long num)
 	{
-		a[i * r + j] = num % MOD;
+		a[i * c + j] = num % MOD;
 		return *this;
 	}
 	Matrix& set(long long *init)
 	{
 		for(int i = 0; i < r; i++)
 			for(int j = 0; j < c; j++)
-				a[i * r + j] = init[i * r + j] % MOD;
+				a[i * c + j] = init[i * r + j] % MOD;
 		return *this;
+	} 
+	long long get(int x, int y)
+	{
+		return a[x * c + y];
 	}
-	Matrix operator * (Matrix& b)
+
+	Matrix operator * (Matrix b)
 	{
 		Matrix res(r, b.c);
 		for(int i = 0; i < r; i++)
-			for(int j = 0; j < c; j++)
-				if(a[i * r + j])
-					for(int k = 0; k < b.c; k++)
-					{
-						long long p = a[i * r + j] * b.a[j * b.r + k];
-						if(p > MOD) p %= MOD;
-						res.a[i * r + k] += p;
-						if(res.a[i * r + k] > MOD) res.a[i * r + k] %= MOD;
-					}		
+			for(int j = 0; j < b.c; j++)
+			{
+				for(int k = 0; k < c; k++)
+					res.set(i, j, (res.get(i, j) + get(i, k) * b.get(k, j) % MOD) % MOD);
+			}
 		return res;
 	}
 	/*
@@ -58,30 +59,42 @@ struct Matrix
 	*/
 };
 
+ostream& operator << (ostream& out, Matrix& a)
+{
+	for(int i = 0; i < a.r; i++)
+	{
+		for(int j = 0; j < a.c; j++)
+			out << a.a[i * a.c + j] << " ";
+		if(i != a.r - 1) out << endl;
+	}
+	return out;
+}
 
 long long mul(long long p)
 {	
-	p--;
+	if(p == 1) return 1;
+	if(p == 2) return (1 + (a2 * a2) % MOD) % MOD;
+	p--;p--;
 	Matrix x(4, 4);
 	//Matrix one(4, 4);	
 	Matrix res(4, 1);
 	
 	long long init[MAXM] = 	
 	{
-		1, 1, 0, 0,
-		0, 4ll * ((a2 * a2) % MOD), 1, (-4ll * a2) % MOD + MOD,
-		0, 1, 0, 0,
-		0, 0, 2ll * a2, -1 + MOD % MOD
+		1, 4 * a2 % MOD * a2 % MOD, (-4 * a2 + MOD) % MOD, 1,
+		0, (4 * a2) % MOD, (-4 * a2 % MOD) % MOD, 1,
+		0, 0, -1 + MOD, 2 * a2 % MOD,
+		0, 1, 0, 0 
 	};
 	x.set(init);
-	res.set(0, 0, 1).set(1, 0, (a2 * a2) % MOD).set(2, 0, 1).set(3, 0, a2);
+	res.set(0, 0, (1 + (a2 * a2) % MOD) % MOD).set(1, 0, a2 * a2 % MOD).set(2, 0, 0).set(3, 0, 0);
 	while(p)
 	{
 		if(p & 1) res = x * res;
 		x = x * x;
 		p >>= 1;
 	}
-	return res.a[0];
+	return res.a[1];
 }
 
 int main()
