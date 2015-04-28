a9a827c6ed6c69d27a5cb6b5562bbd87ee78512b
Dalu Feng <fengdalu@gmail.com>
2015-04-28 16:44:56 +0800

    Add BJTU 2014

---
+++ BJTU/BJTU 2014 Freshers/1696.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+using namespace std;
+
+string a, b, buf;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);	
+	int T; cin >> T;
+	for(int ii = 0; ii < T; ii++)
+	{
+		cout << "Case #" << ii + 1 << ": ";
+		cin >> buf >> a >> b;
+		if(a == b) cout << "Once again!";
+		else if(a == "scissors" && b == "paper") cout << "I made it!~";
+		else if(a == "rock" && b == "scissors") cout << "I made it!~";
+		else if(a == "paper" && b == "rock") cout << "I made it!~";
+		else cout << "No no no!";
+		cout << endl;	
+	}
+	return 0;
+}
---
+++ BJTU/BJTU 2014 Freshers/1697.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int x, g, h, k;
+int buf[100];
+int n;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	int T; cin >> T;
+	for(int ii = 0; ii < T; ii++)
+	{
+		cin >> x >> b >> h >> k >> n;
+		for(int i = 0; i < n; i++) cin >> buf[i];
+		
+	}
+}
