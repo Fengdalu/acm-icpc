34c871ffddd3a5eb881a96f30c6005932c734788
Dalu Feng <fengdalu@gmail.com>
2015-04-29 15:20:10 +0800

    Add BJTU 1697

--- BJTU/BJTU 2014 Freshers/1697.cpp
+++ BJTU/BJTU 2014 Freshers/1697.cpp
@@ -14,8 +14,16 @@ int main()
 	int T; cin >> T;
 	for(int ii = 0; ii < T; ii++)
 	{
-		cin >> x >> b >> h >> k >> n;
+		cin >> x >> g >> h >> k >> n;
 		for(int i = 0; i < n; i++) cin >> buf[i];
-		
+		int c1 = 0, c2 = 0, c3 = 0;
+		for(int i = 0; i < n; i++)
+		{
+			if(buf[i] <= x + g) c1++;
+			if(buf[i] <= x + h) c2++;
+			if(buf[i] <= x + k) c3++;			
+		}
+		cout << c1 << " " << c2 << " " << c3 << endl;
 	}
+	return 0;
 }
