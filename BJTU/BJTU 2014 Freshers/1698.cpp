cb3b0afeb03c58aad787759d8c28647ee1f489b9
Dalu Feng <fengdalu@gmail.com>
2015-04-30 01:18:03 +0800

    ADD NEW

---
+++ BJTU/BJTU 2014 Freshers/1698.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int n;
+int main()
+{	
+	ios::sync_with_stdio(false);
+	int T; cin >> T; for(int ii = 0; ii < T; ii++)
+	{
+		cin >> n;
+		for(int i = 0; i < n; i++) cout << " ";
+		cout << "^" << endl;
+		for(int i = 1; i < n; i++)
+		{
+			for(int j = 0; j < n - i; j++) cout << " ";
+			cout << "*";
+			for(int j = 0; j < 2 * i - 1; j++) cout << " ";
+			cout << "*" << endl;			
+		}
+		cout << "<";
+		for(int j = 0; j < 2 * n - 1; j++) cout << " ";
+		cout << ">" << endl;
+		for(int i = n - 1; i >= 1; i--)
+		{
+			for(int j = 0; j < n - i; j++) cout << " ";
+			cout << "*";
+			for(int j = 0; j < 2 * i - 1; j++) cout << " ";
+			cout << "*" << endl;			
+		}
+		for(int i = 0; i < n; i++) cout << " ";
+		cout << "v" << endl;
+	}
+	return 0;
+}
