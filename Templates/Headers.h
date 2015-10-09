4c5b7277e418f2d7dd19afdcbfe753d72d95d8cd
Dalu Feng <fengdalu@gmail.com>
2015-10-10 00:09:57 +0800

    modify headers

--- Templates/Headers.h
+++ Templates/Headers.h
@@ -3,10 +3,13 @@
 #include <cstring>
 #include <set>
 #include <map>
+#include <vector>
+#include <algorithm>
 using namespace std;
+#define LL long long
+#define u32  unsigned int
 #define AA first
 #define BB second
-typedef long long LL;
 typedef pair<int, int> PII;
 #define cmin(x, y) x = min(x, y)
 #define cmax(x, y) x = max(x, y)
@@ -19,3 +22,4 @@ typedef pair<int, int> PII;
 #define INS(x) insert(x)
 #define FOR(i, n) for(int i = 0; i < n; i++)
 #define MEM(a) memset(a, 0, sizeof a)
+#define ECH(x) FOR(typeof(x.OP) it; it != x.ED; it++)
