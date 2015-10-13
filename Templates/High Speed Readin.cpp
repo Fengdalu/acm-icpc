36f387bde6ffa53ca16c4ad95dbfe99dcc5dfdac
Dalu Feng <fengdalu@gmail.com>
2015-10-13 20:33:25 +0800

    Add Templates

--- Templates/High Speed Readin.cpp
+++
@@ -1,8 +0,0 @@
-void gn(LL &x){
-    char c;
-    LL sgn=1;
-    while(((c=getchar())<'0'||c>'9') && c!='-');
-    if(c=='-')sgn=-1,x=0;else x=c-'0';
-    while((c=getchar())>='0'&&c<='9')x=x*10+c-'0';
-    x*=sgn;
-}
