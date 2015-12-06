3c2416c5c1d470e965fc37a56274300d36a4a756
Dalu Feng <fengdalu@gmail.com>
2015-12-06 16:51:05 +0800

    Add NEERC 13 H

--- NEERC/2013-2014 ACM-ICPC Northeastern European Regional Contest/H.cpp
+++ NEERC/2013-2014 ACM-ICPC Northeastern European Regional Contest/H.cpp
@@ -3,13 +3,15 @@
 #include <cstring>
 #include <cmath>
 #include <algorithm>
+#include <cassert>
 using namespace std;
 typedef unsigned u32;
-#define Maxn 2000000
+#define Maxn 200000
 #define Mpow 20
 #define MP make_pair
 #define AA first
 #define BB second
+
 
 typedef pair<u32, u32> obj;
 typedef long long LL;
@@ -24,10 +26,12 @@ struct Q {
     int l, r;
     u32 c;
     Q(){}
-    Q(int l, int r, int c): l(l), r(r), c(c) {}
+    Q(int l, int r, u32 c): l(l), r(r), c(c) {}
 }qry[Maxn * 32]; int tot;
 
+
 int cmp(Q a, Q b) {
+    if(a.c == b.c) return MP(a.l, a.r) < MP(b.l, b.r);
     return a.c < b.c;
 }
 
@@ -46,11 +50,15 @@ u32 st(int l, int r) {
 }
 
 int main() {
+    freopen("hack.in", "r", stdin);
+    freopen("hack.out", "w", stdout);
+
     scanf("%d", &n);
     for(int i = 1; i <= n; i++) {
         scanf("%u", &c[i]);
     }
     make();
+    xsum[0] = 0;
     for(int i = 1; i <= n; i++) xsum[i] = xsum[i - 1] ^ c[i];
 
     tot = 0;
@@ -64,20 +72,22 @@ int main() {
                 if(st(i, mid) == s) l = mid;
                 else r = mid - 1;
             }
-            qry[tot++] = Q(i, l, s ^ xsum[i - 1]);
+            qry[tot++] = Q(p, l, s ^ xsum[i - 1]);
+            assert(tot < Maxn * 32);
             p = l + 1;
         }
     }
 
-    for(int i = 1; i <= n; i++) index[i - 1] = MP(c[i], i);
 
+
+    for(int i = 1; i <= n; i++) index[i - 1] = MP(xsum[i], i);
     sort(index, index + n);
-
     LL ans = 0;
     for(int i = 0; i < tot; i++) {
-        int k = lower_bound(index, index + n, MP(qry[i].c, (u32)0)) - index;
-        int l = lower_bound(index + k, index + n, MP((u32)qry[i].c, (u32)qry[i].l)) - index;
-        int r = upper_bound(index + k, index + n, MP((u32)qry[i].c, (u32)qry[i].r)) - index;
+//        cout << qry[i].l << " " << qry[i].r << " " << qry[i].c;
+        int l = lower_bound(index, index + n, MP((u32)qry[i].c, (u32)qry[i].l)) - index;
+        int r = upper_bound(index, index + n, MP((u32)qry[i].c, (u32)qry[i].r)) - index;
+//        cout << " " << (r - l) << endl;
         ans += (LL)(r - l);
     }
     printf("%I64d\n", ans);
