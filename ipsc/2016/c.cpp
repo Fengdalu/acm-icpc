dfcf01c710ba6eace4be4b0c7c7be218fc6312e7
Fengdalu <fengdalu@gmail.com>
2016-06-19 18:02:08 +0800

    add *

--- ipsc/2016/c.cpp
+++ ipsc/2016/c.cpp
@@ -15,6 +15,11 @@ ll g[maxn];
 
 int n;
 
+ll qpow(ll a, ll x) {
+    if(x == 0) return 1;
+    return qpow(a * a % mod, x / 2) * (x & 1 ? a : 1) % mod;
+}
+
 int main()  {
     int __;
     cin >> __;
@@ -24,21 +29,6 @@ int main()  {
     for(int i = 2; i < maxn; i++) inv[i] = inv[i - 1] * inv[i] % mod;
     fact[0] = 1;
     for(int i = 1; i < maxn; i++) fact[i] = fact[i - 1] * i % mod;
-    g[1] = 1;
-    g[2] = 1;
-    for(int i = 3; i <= 10; i++) {
-        g[i] = i;
-        for(int j = 2; i - j >= 2; j++) {
-            ll tot = g[j] + g[i - j];
-            ll t = fact[tot];
-            t = (t * inv[g[j]]) % mod;
-            t = (t * inv[g[i - j]]) % mod;
-            t = (t * g[j]) % mod;
-            t = (t * g[i - j]) % mod;
-            t = (t * (i - j)) % mod;
-            g[i] = (g[i] + t) % mod;
-        }
-    }
 
     while(__--) {
         cin >> n;
@@ -57,19 +47,13 @@ int main()  {
                     vis[k] = true;
                     k = p[k];
                 } while(!vis[k]);
-                if(count == 1) continue;
-                else if(count == 2) a.push_back(1);
-                else a.push_back(count);
+                a.push_back(count);
             }
         ll ans = 0;
-        for(int i = 0; i < a.size(); i++) ans += a[i];
-        if(ans == 0) {
-            cout << ans << endl;
-            continue;
-        }
+        for(int i = 0; i < a.size(); i++) ans += a[i] - 1;
         ans = fact[ans];
-        for(int i = 0; i < a.size(); i++) ans = ans * inv[a[i]] % mod;
-        for(int i = 0; i < a.size(); i++) ans = ans * g[a[i]] % mod;
+        for(int i = 0; i < a.size(); i++) ans = ans * inv[a[i] - 1] % mod;
+        for(int i = 0; i < a.size(); i++) ans = ans * qpow(a[i], max(0, a[i] - 2)) % mod;
         cout << ans << endl;
     }
     return 0;
