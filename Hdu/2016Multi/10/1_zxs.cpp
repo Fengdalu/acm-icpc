1e9f3b64db39649f828b02ef5225b3f90c0f77fa
fengdalu <fengdalu@gmail.com>
2016-08-19 15:39:32 +0800

    添加fastio模板

---
+++ Hdu/2016Multi/10/1_zxs.cpp
@@ -0,0 +1,315 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <vector>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+typedef pair<int, int> PII;
+#define PB(x) push_back(x)
+#define SZ size()
+#define AA first
+#define BB second
+#define MP(x, y) make_pair(x, y)
+namespace fastIO{
+    #define BUF_SIZE 100000
+    #define OUT_SIZE 100000
+    #define ll long long
+    //fread->read
+    bool IOerror=0;
+    inline char nc(){
+        static char buf[BUF_SIZE],*p1=buf+BUF_SIZE,*pend=buf+BUF_SIZE;
+        if (p1==pend){
+            p1=buf; pend=buf+fread(buf,1,BUF_SIZE,stdin);
+            if (pend==p1){IOerror=1;return -1;}
+            //{printf("IO error!\n");system("pause");for (;;);exit(0);}
+        }
+        return *p1++;
+    }
+    inline bool blank(char ch){return ch==' '||ch=='\n'||ch=='\r'||ch=='\t';}
+    inline void read(int &x){
+        bool sign=0; char ch=nc(); x=0;
+        for (;blank(ch);ch=nc());
+        if (IOerror)return;
+        if (ch=='-')sign=1,ch=nc();
+        for (;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+        if (sign)x=-x;
+    }
+    inline void read(ll &x){
+        bool sign=0; char ch=nc(); x=0;
+        for (;blank(ch);ch=nc());
+        if (IOerror)return;
+        if (ch=='-')sign=1,ch=nc();
+        for (;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+        if (sign)x=-x;
+    }
+    inline void read(double &x){
+        bool sign=0; char ch=nc(); x=0;
+        for (;blank(ch);ch=nc());
+        if (IOerror)return;
+        if (ch=='-')sign=1,ch=nc();
+        for (;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+        if (ch=='.'){
+            double tmp=1; ch=nc();
+            for (;ch>='0'&&ch<='9';ch=nc())tmp/=10.0,x+=tmp*(ch-'0');
+        }
+        if (sign)x=-x;
+    }
+    inline void read(char *s){
+        char ch=nc();
+        for (;blank(ch);ch=nc());
+        if (IOerror)return;
+        for (;!blank(ch)&&!IOerror;ch=nc())*s++=ch;
+        *s=0;
+    }
+    inline void read(char &c){
+        for (c=nc();blank(c);c=nc());
+        if (IOerror){c=-1;return;}
+    }
+    //getchar->read
+    inline void read1(int &x){
+        char ch;int bo=0;x=0;
+        for (ch=getchar();ch<'0'||ch>'9';ch=getchar())if (ch=='-')bo=1;
+        for (;ch>='0'&&ch<='9';x=x*10+ch-'0',ch=getchar());
+        if (bo)x=-x;
+    }
+    inline void read1(ll &x){
+        char ch;int bo=0;x=0;
+        for (ch=getchar();ch<'0'||ch>'9';ch=getchar())if (ch=='-')bo=1;
+        for (;ch>='0'&&ch<='9';x=x*10+ch-'0',ch=getchar());
+        if (bo)x=-x;
+    }
+    inline void read1(double &x){
+        char ch;int bo=0;x=0;
+        for (ch=getchar();ch<'0'||ch>'9';ch=getchar())if (ch=='-')bo=1;
+        for (;ch>='0'&&ch<='9';x=x*10+ch-'0',ch=getchar());
+        if (ch=='.'){
+            double tmp=1;
+            for (ch=getchar();ch>='0'&&ch<='9';tmp/=10.0,x+=tmp*(ch-'0'),ch=getchar());
+        }
+        if (bo)x=-x;
+    }
+    inline void read1(char *s){
+        char ch=getchar();
+        for (;blank(ch);ch=getchar());
+        for (;!blank(ch);ch=getchar())*s++=ch;
+        *s=0;
+    }
+    inline void read1(char &c){for (c=getchar();blank(c);c=getchar());}
+    //scanf->read
+    inline void read2(int &x){scanf("%d",&x);}
+    inline void read2(ll &x){
+        #ifdef _WIN32
+            scanf("%I64d",&x);
+        #else
+        #ifdef __linux
+            scanf("%lld",&x);
+        #else
+            puts("error:can't recognize the system!");
+        #endif
+        #endif
+    }
+    inline void read2(double &x){scanf("%lf",&x);}
+    inline void read2(char *s){scanf("%s",s);}
+    inline void read2(char &c){scanf(" %c",&c);}
+    inline void readln2(char *s){gets(s);}
+    //fwrite->write
+    struct Ostream_fwrite{
+        char *buf,*p1,*pend;
+        Ostream_fwrite(){buf=new char[BUF_SIZE];p1=buf;pend=buf+BUF_SIZE;}
+        void out(char ch){
+            if (p1==pend){
+                fwrite(buf,1,BUF_SIZE,stdout);p1=buf;
+            }
+            *p1++=ch;
+        }
+        void print(int x){
+            static char s[15],*s1;s1=s;
+            if (!x)*s1++='0';if (x<0)out('-'),x=-x;
+            while(x)*s1++=x%10+'0',x/=10;
+            while(s1--!=s)out(*s1);
+        }
+        void println(int x){
+            static char s[15],*s1;s1=s;
+            if (!x)*s1++='0';if (x<0)out('-'),x=-x;
+            while(x)*s1++=x%10+'0',x/=10;
+            while(s1--!=s)out(*s1); out('\n');
+        }
+        void print(ll x){
+            static char s[25],*s1;s1=s;
+            if (!x)*s1++='0';if (x<0)out('-'),x=-x;
+            while(x)*s1++=x%10+'0',x/=10;
+            while(s1--!=s)out(*s1);
+        }
+        void println(ll x){
+            static char s[25],*s1;s1=s;
+            if (!x)*s1++='0';if (x<0)out('-'),x=-x;
+            while(x)*s1++=x%10+'0',x/=10;
+            while(s1--!=s)out(*s1); out('\n');
+        }
+        void print(double x,int y){
+            static ll mul[]={1,10,100,1000,10000,100000,1000000,10000000,100000000,
+                1000000000,10000000000LL,100000000000LL,1000000000000LL,10000000000000LL,
+                100000000000000LL,1000000000000000LL,10000000000000000LL,100000000000000000LL};
+            if (x<-1e-12)out('-'),x=-x;x*=mul[y];
+            ll x1=(ll)floor(x); if (x-floor(x)>=0.5)++x1;
+            ll x2=x1/mul[y],x3=x1-x2*mul[y]; print(x2);
+            if (y>0){out('.'); for (size_t i=1;i<y&&x3*mul[i]<mul[y];out('0'),++i); print(x3);}
+        }
+        void println(double x,int y){print(x,y);out('\n');}
+        void print(char *s){while (*s)out(*s++);}
+        void println(char *s){while (*s)out(*s++);out('\n');}
+        void flush(){if (p1!=buf){fwrite(buf,1,p1-buf,stdout);p1=buf;}}
+        ~Ostream_fwrite(){flush();}
+    }Ostream;
+    inline void print(int x){Ostream.print(x);}
+    inline void println(int x){Ostream.println(x);}
+    inline void print(char x){Ostream.out(x);}
+    inline void println(char x){Ostream.out(x);Ostream.out('\n');}
+    inline void print(ll x){Ostream.print(x);}
+    inline void println(ll x){Ostream.println(x);}
+    inline void print(double x,int y){Ostream.print(x,y);}
+    inline void println(double x,int y){Ostream.println(x,y);}
+    inline void print(char *s){Ostream.print(s);}
+    inline void println(char *s){Ostream.println(s);}
+    inline void println(){Ostream.out('\n');}
+    inline void flush(){Ostream.flush();}
+    //puts->write
+    char Out[OUT_SIZE],*o=Out;
+    inline void print1(int x){
+        static char buf[15];
+        char *p1=buf;if (!x)*p1++='0';if (x<0)*o++='-',x=-x;
+        while(x)*p1++=x%10+'0',x/=10;
+        while(p1--!=buf)*o++=*p1;
+    }
+    inline void println1(int x){print1(x);*o++='\n';}
+    inline void print1(ll x){
+        static char buf[25];
+        char *p1=buf;if (!x)*p1++='0';if (x<0)*o++='-',x=-x;
+        while(x)*p1++=x%10+'0',x/=10;
+        while(p1--!=buf)*o++=*p1;
+    }
+    inline void println1(ll x){print1(x);*o++='\n';}
+    inline void print1(char c){*o++=c;}
+    inline void println1(char c){*o++=c;*o++='\n';}
+    inline void print1(char *s){while (*s)*o++=*s++;}
+    inline void println1(char *s){print1(s);*o++='\n';}
+    inline void println1(){*o++='\n';}
+    inline void flush1(){if (o!=Out){if (*(o-1)=='\n')*--o=0;puts(Out);}}
+    struct puts_write{
+        ~puts_write(){flush1();}
+    }_puts;
+    inline void print2(int x){printf("%d",x);}
+    inline void println2(int x){printf("%d\n",x);}
+    inline void print2(char x){printf("%c",x);}
+    inline void println2(char x){printf("%c\n",x);}
+    inline void print2(ll x){
+        #ifdef _WIN32
+            printf("%I64d",x);
+        #else
+        #ifdef __linux
+            printf("%lld",x);
+        #else
+            puts("error:can't recognize the system!");
+        #endif
+        #endif
+    }
+    inline void println2(ll x){print2(x);printf("\n");}
+    inline void println2(){printf("\n");}
+    #undef ll
+    #undef OUT_SIZE
+    #undef BUF_SIZE
+};
+using namespace fastIO;
+
+const int N = 100005;
+struct E {
+    int l, r;
+    int sum;
+}e[N<<5];
+int rt[N];
+int X[N];
+int w[N];
+int tot;
+
+inline int get(int pos) {
+    if(pos > tot || pos == -1) {
+        tot++;
+        e[tot].l = e[tot].r = -1;
+        e[tot].sum = 0;
+        return tot;
+    }
+    return pos;
+}
+
+void update(int &rt, int l, int r, int w, int fa) {
+    rt = tot++;
+    e[rt].l = e[fa].l;
+    e[rt].r = e[fa].r;
+    e[rt].sum = e[fa].sum + 1;
+    if(l == r) return ;
+    int mid = (l + r) >> 1;
+    if(w <= mid) update(e[rt].l, l, mid, w, get(e[fa].l));
+    else update(e[rt].r, mid + 1, r, w, get(e[fa].r));
+}
+
+double query(int l1, int r1, int l2, int r2, int l, int r, int k) {
+    int mid = (l + r) >> 1;
+    int hf = e[get(e[r1].l)].sum + e[get(e[r2].l)].sum - e[get(e[l1].l)].sum - e[get(e[l2].l)].sum;
+    if(l == r) return X[l];
+    if(hf >= k) return query(get(e[l1].l), get(e[r1].l), get(e[l2].l), get(e[r2].l), l, mid, k);
+    else return query(get(e[l1].r), get(e[r1].r), get(e[l2].r), get(e[r2].r), mid + 1, r, k - hf);
+}
+
+using namespace fastIO;
+inline void run() {
+    int n, m;
+    read(n); read(m);
+    //scanf("%d%d", &n, &m);
+    for(int i = 1; i <= n; i++) {
+        read(X[i - 1]);
+        //scanf("%d", &X[i - 1]);
+        w[i] = X[i - 1];
+    }
+    sort(X, X + n);
+    int cnt = unique(X, X + n) - X;
+    for(int i = 1; i <= n; i++) w[i] = lower_bound(X, X + cnt, w[i]) - X;
+    tot = 0;
+    memset(rt, -1, sizeof rt);
+    for(int i = 1; i <= n; i++) update(rt[i], 0, cnt - 1, w[i], get(rt[i - 1]));
+    while(m--) {
+        int l1, r1, l2, r2;
+        read(l1); read(r1); read(l2); read(r2);
+        //scanf("%d%d%d%d", &l1, &r1, &l2, &r2);
+        l1--; l2--; l1 = rt[l1], l2 = rt[l2], r1 = rt[r1], r2 = rt[r2];
+        l1 = get(l1), r1 = get(r1), l2 = get(l2), r2 = get(r2);
+        int sum = e[r1].sum + e[r2].sum - e[l1].sum - e[l2].sum;
+        //cout << sum << endl;
+        int now = tot;
+        if(sum & 1) {
+            println((double)query(l1, r1, l2, r2, 0, cnt - 1, sum / 2 + 1), 1);
+            //printf("%.1f\n", (double)query(l1, r1, l2, r2, 0, cnt - 1, sum / 2 + 1));
+        }
+        else {
+            double a = query(l1, r1, l2, r2, 0, cnt - 1, sum / 2);
+            double b = query(l1, r1, l2, r2, 0, cnt - 1, sum / 2 + 1);
+            //cout << a << " " << b << endl;
+            a = (a + b) / 2.0;
+            //printf("%.1f\n", a);
+            println(a, 1);
+        }
+        tot = now;
+    }
+}
+
+int main() {
+    int _T; scanf("%d", &_T);
+    while(_T--) {
+        run();
+    }
+    return 0;
+}
