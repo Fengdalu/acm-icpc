f7dea1f8a01238741d7612dd6c97579160b8fc98
fengdalu <fengdalu@gmail.com>
2016-09-05 00:58:49 +0800

    调整 2015 结构

--- regional/2010/Chengdu/i.cpp
+++ regional/2010/Chengdu/i.cpp
@@ -13,69 +13,67 @@
 #include <string>
 #include <bitset>
 using namespace std;
-int n,k;
-int hp[50005];
-bool vis[50005];
-long long cal(long long p,long long cnt,long long sumk2,long long sumk,long long j)
-{
-    return cnt*p-(sumk2-j*sumk-j*sumk+j*j*cnt);
+typedef long long ll;
+#define AA first
+#define BB second
+#define MP make_pair
+#define PII pair<int, int>
+
+template <class T>
+T SQR(T x) {
+    return x * x;
 }
-bool check(long long p)
-{
-    long long need=0;
-    memset(vis,0,sizeof(vis));
-    queue< pair<long long,pair<long long,long long> > >s;
-    long long d=(long long)ceil(sqrt(p));
-    long long sumk=0;
-    long long sumk2=0;
-    long long cnt=0;
-    for(int i=n-1;i>=0;i--)
-    {
-        if(vis[i])
-        {
-            pair<long long, pair<long long,long long> >head= s.front();
-            long long CNT=head.second.second;
-            long long K=head.second.first;
-            cnt-=CNT;
-            sumk-=CNT*K;
-            sumk2-=CNT*K*K;
-            s.pop();
+
+int n;
+ll m[60000];
+
+ll check(ll p) {
+    ll s1 = 0, s2 = 0, cnt = 0;
+    ll tot = 0;
+    queue< pair<ll, ll> >q;
+    for(ll i = n - 1; i >= 0; i--) {
+        while(q.size()) {
+            ll t = q.front().first;
+            if(p - SQR(t - i) <= 0) {
+                ll cc = q.front().second;
+                s1 -= cc * t;
+                s2 -= cc * SQR(t);
+                cnt -= cc;
+                q.pop();
+            }
+            else break;
         }
-        long long atk=cal(p,cnt,sumk2,sumk,i);
-        if(hp[i]<atk);
-        else
-        {
-            long long res=hp[i]-atk;
-            long long nd=res/p+(res%p==0);
-            need+=nd;
-            sumk+=nd*i;
-            sumk2+=nd*i*i;
-            s.push(make_pair(d-i,make_pair(i,nd)));
-//            s.push(make_pair(i-d,make_pair(i,nd)));
-            if(i-d>=0)vis[i-d]=1;
+        ll atk = cnt * p - s2 + 2ll * i * s1 - cnt * SQR(i);
+        if(atk <= m[i]) {
+            ll _cnt = (m[i] - atk) / p + 1;
+            tot += _cnt;
+            cnt += _cnt;
+            s1 += _cnt * i;
+            s2 += _cnt * SQR(i);
+            q.push(MP(i, _cnt));
         }
     }
-    return need<=k;
+    return tot;
 }
-int main()
-{
-    int T;
-    scanf("%d",&T);
-    while(T--)
-    {
-        scanf("%d%d",&n,&k);
-        for(int i=0;i<n;i++)
-            scanf("%d",&hp[i]);
-        long long L=1,R=(1LL<<62);
-        while(L<R)
-        {
-            long long mid=(L+R)/2;
-            if(check(mid))
-                R=mid;
-            else
-                L=mid+1;
-        }
-        cout << L << endl;
+
+inline void run() {
+    int k;
+    scanf("%d%d", &n, &k);
+    for(int i = 0; i < n; i++) {
+        int _; scanf("%d", &_);
+        m[i] = _;
     }
+    ll l = 1, r = 1ll<<62;
+    while(l < r) {
+        ll mid = (l + r) >> 1;
+        if(check(mid) > k) l = mid + 1;
+        else r = mid;
+    }
+    cout << l << endl;
+}
+
+int main() {
+    int _T; scanf("%d", &_T);
+    while(_T--)  run();
     return 0;
 }
