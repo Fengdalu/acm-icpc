296cb1852f30f154a3a0e3bc5609f1525190a62a
fengdalu <fengdalu@gmail.com>
2016-09-11 20:59:56 +0800

    修改 RMQ 模板

--- _template/DataStructure/RMQ.cpp
+++ _template/DataStructure/RMQ.cpp
@@ -3,7 +3,7 @@
  *
  */
 #include <algorithm>
-using namespace std;
+
 const int N = 1e5;
 const int POW = 32;
 int f[N][POW];
@@ -11,12 +11,11 @@ int mm[N];
 void init(int n) {
     for(int j = 0; j < POW; j++)
         for(int i = 0; i < n; i++)
-            if(i + (1 << (j + 1)) <= n)
-            {
-                f[i][j + 1] = max(f[i][j], f[i + (1 << j)][j]);
+            if(i + (1 << (j + 1)) <= n) {
+                f[i][j + 1] = std::max(f[i][j], f[i + (1 << j)][j]);
             }
     mm[0] = -1;
-    for(int i = 0; i < N; i++) {
+    for(int i = 1; i < N; i++) {
         if((i & (i - 1)) == 0) mm[i] = mm[i - 1] + 1;
         else mm[i] = mm[i - 1];
     }
@@ -24,5 +23,5 @@ void init(int n) {
 
 int cal(int l, int r) {
     int k = mm[r - l + 1];
-    return max(f[l][k], f[r - (1 << k) + 1][k]);
+    return std::max(f[l][k], f[r - (1 << k) + 1][k]);
 }
