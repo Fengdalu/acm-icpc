f106c26061846d86f5a41424072bd8b7a37d498f
fengdalu <fengdalu@gmail.com>
2016-11-21 17:22:11 +0800

    add *

--- BJTU/2014 新生赛/1697.cpp
+++ BJTU/2014 新生赛/1697.cpp
@@ -8,22 +8,19 @@ int x, g, h, k;
 int buf[100];
 int n;
 
-int main()
-{
-	ios::sync_with_stdio(false);
-	int T; cin >> T;
-	for(int ii = 0; ii < T; ii++)
-	{
-		cin >> x >> g >> h >> k >> n;
-		for(int i = 0; i < n; i++) cin >> buf[i];
-		int c1 = 0, c2 = 0, c3 = 0;
-		for(int i = 0; i < n; i++)
-		{
-			if(buf[i] <= x + g) c1++;
-			if(buf[i] <= x + h) c2++;
-			if(buf[i] <= x + k) c3++;			
-		}
-		cout << c1 << " " << c2 << " " << c3 << endl;
-	}
-	return 0;
+int main() {
+  ios::sync_with_stdio(false);
+  int T; cin >> T;
+  for(int ii = 0; ii < T; ii++) {
+    cin >> x >> g >> h >> k >> n;
+    for(int i = 0; i < n; i++) cin >> buf[i];
+    int c1 = 0, c2 = 0, c3 = 0;
+    for(int i = 0; i < n; i++) {
+      if(buf[i] <= x + g) c1++;
+      if(buf[i] <= x + h) c2++;
+      if(buf[i] <= x + k) c3++;			
+    }
+    cout << c1 << " " << c2 << " " << c3 << endl;
+  }
+  return 0;
 }
